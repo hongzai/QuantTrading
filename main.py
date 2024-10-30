f6166da017cf64f20f8af5da82e3bb20599b82d6
HongZai <hongzai@live.com>
2024-10-30 10:36:35 +0800

    #1 Simplied parameter

--- main.py
+++ main.py
@@ -9,21 +9,38 @@ if __name__ == "__main__":
     # Parameter
     coins = [ "BTC" ]
     time_frames = [ "1h" ]
-    models = [ ModelEnum.MEAN ]
+    models = [ ModelEnum.MEAN, ModelEnum.ZSCORE ]
     trading_strategies = [ TradingStrategyEnum.LONG_SHORT_OUTRANGE_MOMEMTUM ]
-    alpha_column_name = "coinbase_premium_gap"
     rolling_windows = [ 100, 200, 300, 400, 500 ]
     diff_thresholds = [ 0.1, 0.2, 0.3, 0.4, 0.5 ]
+    trading_fee = 0.000 # 0.00055
     
+    # Data source
+    alpha_column_name = "coinbase_premium_gap"
+    alpha_data_sources = {
+        coin: {
+            time_frame: f"{parent_dir}/resources/Cryptoquant_{coin}_MarketCoinbasePremiumIndex_{time_frame}.csv"
+            for time_frame in time_frames
+        }
+        for coin in coins
+    }
+    candle_data_source = {
+        coin: {
+            time_frame: f"{parent_dir}/resources/binance_candle_{coin}_{time_frame}.csv"
+            for time_frame in time_frames
+        }
+        for coin in coins
+    }
+
     # Iterate optimization process
     for coin in coins:
         for time_frame in time_frames:
             for model in models:
                 for trading_strategy in trading_strategies:
-                    data_market_coinbase_premium_index = pd.read_csv(f"{parent_dir}/resources/Cryptoquant_{coin}_MarketCoinbasePremiumIndex_{time_frame}.csv")
-                    data_candle = pd.read_csv(f"{parent_dir}/resources/binance_candle_{coin}_{time_frame}.csv")
+                    data_source = pd.read_csv(alpha_data_sources[coin][time_frame])
+                    data_candle = pd.read_csv(candle_data_source[coin][time_frame])
 
-                    optimization = Optimization(data_sources=[data_market_coinbase_premium_index], 
+                    optimization = Optimization(data_sources=[data_source], 
                                 data_candle=data_candle,
                                 rolling_windows=rolling_windows, 
                                 diff_thresholds=diff_thresholds, 
@@ -32,9 +49,11 @@ if __name__ == "__main__":
                                 model=model,
                                 time_frame=time_frame,
                                 alpha_column_name=alpha_column_name,
-                                trading_fee= 0.000, # 0.00055
+                                trading_fee=trading_fee,
                                 output_folder=f"{parent_dir}/output",
                                 export_file_name=f"{alpha_column_name}_{model.name}_{alpha_column_name}_{coin.upper()}_{time_frame}",
                                 is_export_all_chart=False,
                                 is_export_all_csv=False)
                     optimization.run()
+                    
+                    
